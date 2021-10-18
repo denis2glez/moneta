c4c18f71fa881eee10a5ade5d786597476c423fd
Denis Deniz González <denis2.glez@gmail.com>
2021-10-18 13:49:24 +0200

    Add a second database migration that creates the table `crypto_latest_listings`.

---
+++ coin-market-cap/migrations/20211018112204_create_cryto_listings_latest_table.sql
@@ -0,0 +1,30 @@
+-- Create a table with the latest listings of cryptocurrencies
+
+CREATE TABLE crypto_latest_listings (
+    id INTEGER REFERENCES crypto_map,
+    num_market_pairs INTEGER,
+    tags TEXT[],
+    max_supply INTEGER,
+    circulating_supply INTEGER,
+    total_supply INTEGER,
+    platform INTEGER REFERENCES platforms (id),
+    cmc_rank INTEGER,
+
+    quote TEXT,    
+    -- Currently, the changes are represented in-place.
+    price NUMERIC,
+    volume_24h NUMERIC,
+    volume_change_24h NUMERIC,
+    percent_change_1h NUMERIC,
+    percent_change_24h NUMERIC,
+    percent_change_7d NUMERIC,
+    percent_change_30d NUMERIC,
+    percent_change_60d NUMERIC,
+    percent_change_90d NUMERIC,
+    market_cap NUMERIC,
+    market_cap_dominance NUMERIC,
+    fully_diluted_market_cap NUMERIC,
+    last_updated timestamptz NOT NULL,
+
+    PRIMARY KEY (id, last_updated)
+);
